11be7c3f981ad4b4b87b010e3da93e2613368449
JurisNexaIt <shubham.developerjuris@gmail.com>
2026-05-06 00:57:36 +0500

    Update ContactForm.jsx

--- src/Components/ContactForm.jsx
+++ src/Components/ContactForm.jsx
@@ -79,7 +79,7 @@ const ContactForm = () => {
               <Phone className="w-6 h-6 text-red-500" /> +1 365-363-6067
             </li>
             <li className="flex items-center gap-3 text-gray-200 font-medium text-lg">
-              <MapPin className="w-6 h-6 text-red-500" /> Unit 307-B , 3016 5 Ave NE, Calgary, AB T2A 5L7
+              <MapPin className="w-6 h-6 text-red-500" /> 12885 80 Ave Unit 201, Surrey, BC V3W 0K8
             </li>
           </ul>
         </motion.div>
